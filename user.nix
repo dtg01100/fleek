8a98474555daf9657db59ba6d2dd49e0943d0170
David LaFreniere <dlafreniere349@gmail.com>
2024-01-22 17:39:29 -0500

    fleek: apply

--- user.nix
+++ user.nix
@@ -1,5 +1,9 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
-
+programs.bash.shellAliases = {
+  aliastest = "ls";
+  ll = "ls -l";
+  ls = "ls --color=tty";
+};
  
 }
